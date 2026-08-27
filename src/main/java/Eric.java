b8bdb5a20a5c4d256640e1292a1b5cb37506e76f
Rachel Li <rachel@Rachels-MacBook-Pro-9.local>
2026-08-27 14:17:54 +0800

    Level-0

---
+++ src/main/java/Eric.java
@@ -0,0 +1,18 @@
+public class Eric {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        String banner = " _____      _        \n"
+                + "| ____|_ __(_) ___   \n"
+                + "|  _| | '__| |/ __|  \n"
+                + "| |___| |  | | (__   \n"
+                + "|_____|_|  |_|\\___|  \n";
+        System.out.println(banner);
+
+        System.out.println("Hello! I'm Eric.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
