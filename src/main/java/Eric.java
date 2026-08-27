02a293ffd0f7121e0233b2402f7a645aab8c1526
Rachel Li <rachel@Rachels-MacBook-Pro-9.local>
2026-08-27 15:30:07 +0800

    implemented Echo

--- src/main/java/Eric.java
+++ src/main/java/Eric.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Eric {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -11,6 +13,20 @@ public class Eric {
 
         System.out.println("Hello! I'm Eric.");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = in.nextLine();
+        }
+
+        in.close();
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
